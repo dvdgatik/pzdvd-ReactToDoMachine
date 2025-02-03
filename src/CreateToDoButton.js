8295e803584a77dd42d096600d0f2cca82a0c135
David Gatica <dvd_gatik@hotmail.com>
2025-02-03 00:13:02 -0600

    Event onChange and onClick

--- src/CreateToDoButton.js
+++ src/CreateToDoButton.js
@@ -1,10 +1,20 @@
 import './CreateToDoButton.css';
+
+// Capturar los eventos del usuario
+// El onclick debe ser con camelCase onClick, todo lo que empieze en on va a ser un evento
+// todo eso lo va a escuchar en un addEventListener (En un escuchador de eventos) para detetar la interaccion del usuario
+//Los eventos debemos encapsularlos en una funcion
+// en el navegador no existe el onClick
+// React tradujo el "atributo" onclick a eventos de JavaScript
+// en la funcion donde recibimos el escuchador de eventos podemos recibir el evento como parametro
+// target: hace referencia al elemento html donde se origino el evento, devuelve el elemento html
+// al obtener el evento recibiras un  objeto llamado SyntheticBaseEvent que guarda informacion del evento una de ellas target
+
 function CreateToDoButton() {
     //Elementos de react
     return(
-        <button className='CreateToDoButton'> +</button>
+        <button onClick={(event)=>console.log('Le diste Click', event.target)} className='CreateToDoButton'> +</button>
     );
 }
-
 
 export { CreateToDoButton }
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,7 +1,11 @@
 import './TodoSearch.css'
 function TodoSearch() {
     return (
-        <input  className='TodoSearch' placeholder="Cortar Cebolla"/>
+        <input 
+            className='TodoSearch'
+            placeholder="Cortar Cebolla"
+            onChange={e=>{console.log(e.target.value)}}
+        />
     );
 }
 
